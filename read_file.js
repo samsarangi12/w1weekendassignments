fd9e1acccb7309cecf3a940f01b2aad01524262a
samsarangi12 <soumyashree.sarangi@gmail.com>
2019-04-07 18:58:58 +0000

    File IO Exercise Assignment

---
+++ read_file.js
@@ -0,0 +1,28 @@
+var inputFilePath = process.argv.slice(2);
+var fs = require("fs");
+var buf = new Buffer(1024);
+
+console.log("Going to open an existing file");
+fs.open(inputFilePath[0], 'r+', function (err, fd) {
+  if(err) {
+    return console.error(err);
+  }
+  console.log("File opened successfully");
+  console.log("Going to read the opened file");
+
+  fs.read(fd, buf, 0, buf.length, 0, function (err, bytes) {
+
+    if (err) {
+      console.log(err)
+    }
+    console.log(bytes + " bytes read");
+
+    if (bytes > 0) {
+      console.log(buf.slice(0, bytes).toString())
+    }
+  });
+
+});
+
+
+
